7b927d6cdda285b790d27fa8037820f822b2c76e
sujata <sujatadhere8@gmail.com>
2025-10-07 11:25:52 +0530

    Spring boot user application

---
+++ usermanagement/src/main/java/com/ty/usermanagement/UserController.java
@@ -0,0 +1,28 @@
+package com.ty.usermanagement;
+
+import java.util.ArrayList;
+import java.util.List;   
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private List<User> users = new ArrayList<>();
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return users;
+    }
+
+    @PostMapping
+    public String addUser(@RequestBody User user) {
+        users.add(user);
+        return "User added successfully!";
+    }
+}
